deef2e1865b69e8e604981dade11810b3a864029
hui.kang <kangpangpang@gmail.com>
2013-08-30 22:51:21 +0800

    U: add totoro log

--- static/labor.js
+++ static/labor.js
@@ -247,4 +247,15 @@
         }
     }
 
+    var totoro = window.totoro || (window.totoro = {})
+
+    // test output
+    totoro.log = function(data) {
+        var id = location.href.match(/runner\/([^/]+)\//)[1]
+        window.report({
+            orderId: id,
+            action: 'debug',
+            info: data
+        })
+    }
 })()
